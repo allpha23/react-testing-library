d6254fe43a73270ba5635785c38486760421ed63
allpha23 <allpha23@gmail.com>
2021-11-05 18:00:34 -0300

    Requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente NotFound.js', () => {
+  test('Teste se contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundEl = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notFoundEl).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const imgEl = screen.getByRole('img', { name: /not found/i });
+    expect(imgEl).toBeInTheDocument();
+    expect(imgEl).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
